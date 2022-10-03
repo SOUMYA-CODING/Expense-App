2b472a62c15fa32d121493c354e375e0f8ffa9f3
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-10-03 21:38:51 +0530

    Update

---
+++ app/src/main/java/com/example/expenseapp/welcomescreen/WelcomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.expenseapp.welcomescreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.expenseapp.R
+
+class WelcomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+    }
+}
