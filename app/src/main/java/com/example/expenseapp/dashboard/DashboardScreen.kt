32b65ac80f936f08b33918ebe48a463b9b807ac7
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-10-03 21:37:27 +0530

    Update

--- app/src/main/java/com/example/expenseapp/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/example/expenseapp/dashboard/DashboardScreen.kt
@@ -1,11 +1,12 @@
-package com.example.expenseapp
+package com.example.expenseapp.dashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.expenseapp.R
 
-class MainActivity : AppCompatActivity() {
+class DashboardScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dashboard_screen)
     }
 }
